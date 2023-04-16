0b3ec9274669ab5c8907239571bde8bb6f7cc20c
brambooo <bram_reinold@hotmail.com>
2023-04-16 09:30:38 +0200

    Menu & MenuItems added

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+"use client";
+
+import React, { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { MenuItem } from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // TODO: Why usecallback?
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -23,6 +33,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
+          onClick={toggleOpen}
           className="
           p-4
           md:py-1
@@ -44,6 +55,28 @@ const UserMenu = () => {
             <Avatar src={undefined} />
           </div>
         </div>
+
+        {isOpen && (
+          <div
+            className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+          >
+            <div className="flex flex-col cursor-pointer">
+              <MenuItem onClick={() => {}} label="Login" />
+              <MenuItem onClick={() => {}} label="Sign up" />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
