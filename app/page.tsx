1e362f4fb8b2631dce49ce78d76f9ace7b8f3963
brambooo <bram_reinold@hotmail.com>
2023-04-30 13:18:17 +0200

    LstingCard added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
+import getListings, { IListingsParams } from "./actions/getListings";
+import { ClientOnly } from "./components/ClientOnly";
 import Container from "./components/Container";
 
-export default function Home() {
+import React from "react";
+import { EmptyState } from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
+
+interface HomeProps {
+  searchParams: IListingsParams;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <>
-      <Container>Dit is de container content.</Container>
-    </>
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+               pt-24
+               grid 
+               grid-cols-1 
+               sm:grid-cols-2 
+               md:grid-cols-3 
+               lg:grid-cols-4
+               xl:grid-cols-5
+               2xl:grid-cols-6
+               gap-8
+        "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
